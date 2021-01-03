bef333505b8359fca40f4577d92d6c3303bedc75
neuring <neuring@pm.me>
2021-01-03 12:39:03 +0100

    implemented ConstraintRepr for If

--- src/constraints/conditional.rs
+++ src/constraints/conditional.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use std::fmt::Debug;
 
-use crate::{Constraint, ConstraintRepr, Encoder, Lit, SatVar, Solver, VarMap};
+use crate::{clause, Constraint, ConstraintRepr, Encoder, Lit, SatVar, Solver, VarMap};
 
 use super::util;
 
@@ -28,18 +28,68 @@ where
     }
 }
 
+impl<V, C, T> ConstraintRepr<V> for If<C, T>
+where
+    V: SatVar,
+    C: ConstraintRepr<V>,
+    T: ConstraintRepr<V>,
+{
+    fn encode_constraint_implies_repr<S: Solver>(
+        self,
+        repr: Option<i32>,
+        solver: &mut S,
+        varmap: &mut VarMap<V>,
+    ) -> i32 {
+        let repr = repr.unwrap_or_else(|| varmap.new_var());
+
+        let cond_repr = self
+            .cond
+            .encode_constraint_equals_repr(None, solver, varmap);
+        let then_repr = self
+            .then
+            .encode_constraint_equals_repr(None, solver, varmap);
+
+        solver.add_clause(clause![cond_repr, repr]);
+        solver.add_clause(clause![-cond_repr, -then_repr, repr]);
+
+        repr
+    }
+
+    fn encode_constraint_equals_repr<S: Solver>(
+        self,
+        repr: Option<i32>,
+        solver: &mut S,
+        varmap: &mut VarMap<V>,
+    ) -> i32 {
+        let repr = repr.unwrap_or_else(|| varmap.new_var());
+
+        let cond_repr = self
+            .cond
+            .encode_constraint_equals_repr(None, solver, varmap);
+        let then_repr = self
+            .then
+            .encode_constraint_equals_repr(None, solver, varmap);
+
+        solver.add_clause(clause![cond_repr, repr]);
+        solver.add_clause(clause![-cond_repr, -then_repr, repr]);
+
+        solver.add_clause(clause![then_repr, -cond_repr, -repr]);
+
+        repr
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{
-        constraints::{test_util::retry_until_unsat, AtMostK},
-        prelude::*,
-    };
+    use num_integer::binomial;
+
+    use crate::{constraints::{AtMostK, test_util::{constraint_equals_repr_tester, constraint_implies_repr_tester, retry_until_unsat}}, prelude::*};
 
     use super::*;
 
     #[test]
     fn if_then_simple() {
-        let mut encoder = Encoder::<_, cadical::Solver>::new();
+        let mut encoder = DefaultEncoder::new();
 
         let cond = Pos(5);
         let then = Pos(6);
@@ -57,7 +107,7 @@ mod tests {
 
     #[test]
     fn if_then_with_constraints() {
-        let mut encoder = Encoder::<_, cadical::Solver>::new();
+        let mut encoder = DefaultEncoder::new();
 
         let cond = AtMostK {
             k: 2,
@@ -77,5 +127,81 @@ mod tests {
             }
         });
         assert_eq!(r, 110 /*TODO: Verify this is correct */);
+    }
+
+    #[test]
+    fn if_then_implies_repr() {
+        let mut encoder = DefaultEncoder::new();
+
+        let range = 5;
+        let k = 3;
+        let cond = AtMostK {
+            k,
+            lits: (0..range).map(Pos),
+        };
+        let then = Pos(range);
+        let constraint = If { cond, then };
+
+        let repr = constraint.encode_constraint_implies_repr(
+            None,
+            &mut encoder.solver,
+            &mut encoder.varmap,
+        );
+
+        let r = constraint_implies_repr_tester(&mut encoder, repr, |model| {
+            let cond = model
+                .vars()
+                .filter(|l| (0..range).contains(&l.unwrap()))
+                .filter(|l| matches!(l, Pos(_)))
+                .count()
+                <= k as usize;
+            let then = model.var(range).unwrap();
+
+            !cond || then
+        });
+        assert_eq!(
+            r.correct as u32,
+            (0..=k).map(|i| binomial(range, i)).sum::<u32>()
+                + 2 * ((k + 1)..=range).map(|i| binomial(range, i)).sum::<u32>()
+        );
+        assert_eq!(r.total(), 1 << (range + 1));
+    }
+
+    #[test]
+    fn if_then_equals_repr() {
+        let mut encoder = DefaultEncoder::new();
+
+        let range = 5;
+        let k = 3;
+        let cond = AtMostK {
+            k,
+            lits: (0..range).map(Pos),
+        };
+        let then = Pos(range);
+        let constraint = If { cond, then };
+
+        let repr = constraint.encode_constraint_equals_repr(
+            None,
+            &mut encoder.solver,
+            &mut encoder.varmap,
+        );
+
+        let r = constraint_equals_repr_tester(&mut encoder, repr, |model| {
+            let cond = model
+                .vars()
+                .filter(|l| (0..range).contains(&l.unwrap()))
+                .filter(|l| matches!(l, Pos(_)))
+                .count()
+                <= k as usize;
+            let then = model.var(range).unwrap();
+
+            !cond || then
+        });
+        assert_eq!(
+            r.correct as u32,
+            (0..=k).map(|i| binomial(range, i)).sum::<u32>()
+                + 2 * ((k + 1)..=range).map(|i| binomial(range, i)).sum::<u32>()
+        );
+        assert_eq!(r.total(), 1 << (range + 1));
     }
 }
