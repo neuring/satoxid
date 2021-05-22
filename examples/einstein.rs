d85bc64660fc49ec0a7fcb9950752b7908a5b2ba
neuring <neuring@pm.me>
2021-05-22 18:44:01 +0200

    Use generic functions instead of macros in einstein example

--- examples/einstein.rs
+++ examples/einstein.rs
@@ -1,8 +1,6 @@
-use sat_encoder::{
-    constraints::{AtLeastK, ExactlyK, If, Or},
-    Backend, CadicalEncoder, Encoder,
-};
-use strum::IntoEnumIterator;
+use std::fmt;
+
+use sat_encoder::{Backend, CadicalEncoder, Encoder, Model, constraints::{AtLeastK, ExactlyK, If, Or}};
 
 // Implementation of Einstein's Puzzle.
 // https://web.stanford.edu/~laurik/fsmbook/examples/Einstein'sPuzzle.html
@@ -52,7 +50,7 @@ enum Pet {
     Horses,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, derive_more::From)]
 enum Property {
     Color(Color),
     Nationality(Nationality),
@@ -67,102 +65,111 @@ struct House {
     property: Property,
 }
 
-macro_rules! every_house_has_one_of_each_property {
-    ($prop:ident, $encoder:expr) => {
-        for house_number in 0..5 {
-            let lits = $prop::iter()
-                .map(Property::$prop)
-                .map(|property| House {
-                    number: house_number,
-                    property,
-                });
-            $encoder.add_constraint(ExactlyK { k: 1, lits });
-        }
-    };
+fn encode_every_house_has_one_of_each_property<P, B>(encoder: &mut Encoder<House, B>)
+where
+    P: Into<Property> + strum::IntoEnumIterator,
+    <P as strum::IntoEnumIterator>::Iterator: Clone,
+    B: Backend,
+{
+    for house_number in 0..5 {
+        let lits = P::iter().map(Into::into).map(|property| House {
+            number: house_number,
+            property,
+        });
+        encoder.add_constraint(ExactlyK { k: 1, lits });
+    }
 }
 
-macro_rules! every_property_value_appears_once {
-    ($prop:ident, $encoder:expr) => {
-        for p in $prop::iter() {
-            let lits = (0..5)
-                .map(|number| House {
-                    number,
-                    property: Property::$prop(p),
-                });
-            $encoder.add_constraint(AtLeastK { k: 1, lits });
-        }
-    };
+fn encode_every_property_value_appears_once<P, B>(encoder: &mut Encoder<House, B>)
+where
+    P: Into<Property> + strum::IntoEnumIterator,
+    <P as strum::IntoEnumIterator>::Iterator: Clone,
+    B: Backend,
+{
+    for property in P::iter() {
+        let property = property.into();
+        let lits = (0..5).map(|number| House { number, property });
+        encoder.add_constraint(AtLeastK { k: 1, lits });
+    }
 }
 
 fn encode_general_rules(encoder: &mut Encoder<House, impl Backend>) {
     // Every house as exactly one of each of the five properties.
-    every_house_has_one_of_each_property!(Color, encoder);
-    every_house_has_one_of_each_property!(Nationality, encoder);
-    every_house_has_one_of_each_property!(Pet, encoder);
-    every_house_has_one_of_each_property!(Drink, encoder);
-    every_house_has_one_of_each_property!(Cigarette, encoder);
+    encode_every_house_has_one_of_each_property::<Color, _>(encoder);
+    encode_every_house_has_one_of_each_property::<Nationality, _>(encoder);
+    encode_every_house_has_one_of_each_property::<Pet, _>(encoder);
+    encode_every_house_has_one_of_each_property::<Drink, _>(encoder);
+    encode_every_house_has_one_of_each_property::<Cigarette, _>(encoder);
 
     // Every value of each property only appears once.
-    every_property_value_appears_once!(Color, encoder);
-    every_property_value_appears_once!(Nationality, encoder);
-    every_property_value_appears_once!(Pet, encoder);
-    every_property_value_appears_once!(Drink, encoder);
-    every_property_value_appears_once!(Cigarette, encoder);
+    encode_every_property_value_appears_once::<Color, _>(encoder);
+    encode_every_property_value_appears_once::<Nationality, _>(encoder);
+    encode_every_property_value_appears_once::<Pet, _>(encoder);
+    encode_every_property_value_appears_once::<Drink, _>(encoder);
+    encode_every_property_value_appears_once::<Cigarette, _>(encoder);
 }
 
-macro_rules! if_then_constraint {
-    ($ifprop:ident :: $ifvalue:ident, $thenprop:ident :: $thenvalue:ident, $encoder:expr) => {
-        for number in 0..5 {
-            $encoder.add_constraint(If {
-                cond: House {
-                    number,
-                    property: Property::$ifprop($ifprop::$ifvalue),
-                },
-                then: House {
-                    number,
-                    property: Property::$thenprop($thenprop::$thenvalue),
-                },
-            })
-        }
-    };
+fn encode_if_then_constraint(
+    encoder: &mut Encoder<House, impl Backend>,
+    if_prop: impl Into<Property>,
+    then_prop: impl Into<Property>,
+) {
+    let if_prop = if_prop.into();
+    let then_prop = then_prop.into();
+
+    for number in 0..5 {
+        encoder.add_constraint(If {
+            cond: House {
+                number,
+                property: if_prop,
+            },
+            then: House {
+                number,
+                property: then_prop,
+            },
+        })
+    }
 }
 
-macro_rules! neighbor_if_then_constraint {
-    ($ifprop:ident :: $ifvalue:ident, $thenprop:ident :: $thenvalue:ident, $encoder:expr) => {
-        for number in 0..5 {
-            let neighbors = [-1i32, 1]
-                .iter()
-                .map(|i| number + i)
-                .filter(|&i| 0 <= i && i < 5)
-                .map(|neighbor_number| {
-                    House {
-                        number: neighbor_number as u32,
-                        property: Property::$thenprop($thenprop::$thenvalue),
-                    }
-                });
+fn encode_neighbor_if_then_constraint(
+    encoder: &mut Encoder<House, impl Backend>,
+    if_prop: impl Into<Property>,
+    then_prop: impl Into<Property>,
+) {
+    let if_prop = if_prop.into();
+    let then_prop = then_prop.into();
 
-            let number = number as u32;
-
-            $encoder.add_constraint(If {
-                cond: House {
-                    number,
-                    property: Property::$ifprop($ifprop::$ifvalue),
-                },
-                then: Or(neighbors),
+    for number in 0..5 {
+        let neighbors = [-1i32, 1]
+            .iter()
+            .map(|i| number + i)
+            .filter(|&i| 0 <= i && i < 5)
+            .map(|neighbor_number| House {
+                number: neighbor_number as u32,
+                property: then_prop,
             });
-        }
-    };
+
+        let number = number as u32;
+
+        encoder.add_constraint(If {
+            cond: House {
+                number,
+                property: if_prop,
+            },
+            then: Or(neighbors),
+        });
+    }
 }
 
 fn encode_specific_rules(encoder: &mut Encoder<House, impl Backend>) {
     // 1. The Englishman lives in the red house.
-    if_then_constraint!(Nationality::English, Color::Red, encoder);
+    encode_if_then_constraint(encoder, Nationality::English, Color::Red);
 
     // 2. The Swede keeps dogs.
-    if_then_constraint!(Nationality::Swedish, Pet::Dogs, encoder);
+    encode_if_then_constraint(encoder, Nationality::Swedish, Pet::Dogs);
 
     // 3. The Dane drinks tea.
-    if_then_constraint!(Nationality::Danish, Drink::Tea, encoder);
+    encode_if_then_constraint(encoder, Nationality::Danish, Drink::Tea);
 
     // 4. The green house is just to the left of the white one.
     for number in 0..4 {
@@ -179,13 +186,13 @@ fn encode_specific_rules(encoder: &mut Encoder<House, impl Backend>) {
     }
 
     // 5. The owner of the green house drinks coffee.
-    if_then_constraint!(Color::Green, Drink::Coffee, encoder);
+    encode_if_then_constraint(encoder, Color::Green, Drink::Coffee);
 
     // 6. The Pall Mall smoker keeps birds.
-    if_then_constraint!(Cigarette::PallMall, Pet::Birds, encoder);
+    encode_if_then_constraint(encoder, Cigarette::PallMall, Pet::Birds);
 
     // 7. The owner of the yellow house smokes Dunhills.
-    if_then_constraint!(Color::Yellow, Cigarette::Dunhills, encoder);
+    encode_if_then_constraint(encoder, Color::Yellow, Cigarette::Dunhills);
 
     // 8. The man in the center house drinks milk.
     encoder.add_constraint(House {
@@ -200,38 +207,34 @@ fn encode_specific_rules(encoder: &mut Encoder<House, impl Backend>) {
     });
 
     // 10. The Blend smoker has a neighbor who keeps cats.
-    neighbor_if_then_constraint!(Cigarette::Blend, Pet::Cats, encoder);
+    encode_neighbor_if_then_constraint(encoder, Cigarette::Blend, Pet::Cats);
 
     // 11. The man who smokes Blue Masters drinks bier.
-    if_then_constraint!(Cigarette::BlueMasters, Drink::Bier, encoder);
+    encode_if_then_constraint(encoder, Cigarette::BlueMasters, Drink::Bier);
 
     // 12. The man who keeps horses lives next to the Dunhill smoker.
-    neighbor_if_then_constraint!(Pet::Horses, Cigarette::Dunhills, encoder);
+    encode_neighbor_if_then_constraint(encoder, Pet::Horses, Cigarette::Dunhills);
 
     // 13. The German smokes Prince.
-    if_then_constraint!(Nationality::German, Cigarette::Prince, encoder);
+    encode_if_then_constraint(encoder, Nationality::German, Cigarette::Prince);
 
     // 14. The Norwegian lives next to the blue house.
-    neighbor_if_then_constraint!(Nationality::Norwegian, Color::Blue, encoder);
+    encode_neighbor_if_then_constraint(encoder, Nationality::Norwegian, Color::Blue);
 
     // 15. The Blend smoker has a neighbor who drinks water.
-    neighbor_if_then_constraint!(Cigarette::Blend, Drink::Water, encoder);
+    encode_neighbor_if_then_constraint(encoder, Cigarette::Blend, Drink::Water);
 }
 
-macro_rules! print_property {
-    ($prop:ident, $number:expr, $model:expr) => {
-        for p in $prop::iter() {
-            if $model
-                .var(House {
-                    number: $number,
-                    property: Property::$prop(p),
-                })
-                .unwrap()
-            {
-                print!(", {:?}", p);
-            }
+fn print_property<P>(number: u32, model: &Model<House>)
+where
+    P: Into<Property> + strum::IntoEnumIterator + fmt::Debug + Copy,
+{
+    for p in P::iter() {
+        let property = p.into();
+        if model.var(House { number, property }).unwrap() {
+            print!(", {:?}", p);
         }
-    };
+    }
 }
 
 fn main() {
@@ -245,11 +248,11 @@ fn main() {
         for number in 0..5 {
             print!("{}", number);
 
-            print_property!(Color, number, model);
-            print_property!(Nationality, number, model);
-            print_property!(Pet, number, model);
-            print_property!(Drink, number, model);
-            print_property!(Cigarette, number, model);
+            print_property::<Color>(number, &model);
+            print_property::<Nationality>(number, &model);
+            print_property::<Pet>(number, &model);
+            print_property::<Drink>(number, &model);
+            print_property::<Cigarette>(number, &model);
 
             println!();
         }
